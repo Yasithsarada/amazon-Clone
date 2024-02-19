76207b04112d9ee9ea8015b97d9bee5c88ae18fa
Yasith_99 <94992009+Yasithsarada@users.noreply.github.com>
2024-02-19 23:59:07 +0530

    Update addressScreen.dart

--- lib/features/address/screen/addressScreen.dart
+++ lib/features/address/screen/addressScreen.dart
@@ -7,7 +7,7 @@ class AddressScreen extends StatefulWidget {
   static const String routeName = '/address';
   final String totalAmount;
   const AddressScreen({super.key , required this.totalAmount,});
-//
+
   @override
   State<AddressScreen> createState() => _AddressScreenState();
 }
